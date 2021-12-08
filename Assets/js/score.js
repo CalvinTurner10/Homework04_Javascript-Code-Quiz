2f479d009f3118d3830d8b6cdeab91a036cb4fd2
Calvin Turner <turnerjrcalvin@gmail.com>
2021-12-08 18:22:21 -0500

    go back function

---
+++ Assets/js/score.js
@@ -0,0 +1,8 @@
+//The event listen will return to beginniong of code quiz challenge when the user clicks the button
+
+document.getElementById("go-back").addEventListener("click", function show(){
+    const pages = document.getElementsByClassName("page");
+    for (let i = 0; i < pages.length; i++){
+        pages[i].style.display = "none";
+        one.style.display = "block"}});
+    
